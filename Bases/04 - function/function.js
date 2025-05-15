d66492223e65c6e4157e85c2d810392ca33ccc02
JONASJMX <jonasjmx@gmail.com>
2025-05-15 12:05:50 -0500

    Clases y Ejericcios

---
+++ Bases/04 - function/function.js
@@ -0,0 +1,4 @@
+// Ejemplo de función en JavaScript
+function myFunction2() { // Definición de la función
+  document.getElementById("demo2").innerHTML = "Paragraph changed."; // Cambia el contenido del elemento con id "demo2" y lo hace con innerHTML
+}
